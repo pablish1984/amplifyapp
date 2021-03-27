491249094252158c620ed5d862aea20eb47bdca8
Pablo Balboa <pablish1984@gmail.com>
2021-03-27 00:18:21 -0700

    fifth commit- checking why can't access aws-exports file

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import Amplify from 'aws-amplify';
-import config from './aws-exports';
+import config from './aws-exports.js';
 Amplify.configure(config);
 
 ReactDOM.render(
